2226aaf915ddfc697bd070abeb7c630f45755561
Karlsson0214 <karlsson0214@gmail.com>
2022-08-31 20:08:41 +0200

    Can only jump and move if on a brick, not when in the air.

--- Assets/Scripts/Cat.cs
+++ Assets/Scripts/Cat.cs
@@ -6,6 +6,9 @@ public class Cat : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float maxJumpVelocity = 20;
+    private float halfCatHeight = 0.5f;
+    public bool onGround = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,21 +20,54 @@ public class Cat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         // jump
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && onGround)
         {
+
             rb.velocity = new Vector2(rb.velocity.x, maxJumpVelocity);
         }
-        if (Input.GetKey(KeyCode.A))
+
+        if (Input.GetKey(KeyCode.A) && onGround)
         {
             // move left
             rb.velocity = new Vector2(-5, rb.velocity.y);
         }
-        else if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D) && onGround)
         {
             // move right
             rb.velocity = new Vector2(5, rb.velocity.y);
         }
-        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.name.StartsWith("Brick"))
+        {
+            if(collision.collider.transform.position.y < rb.position.y - halfCatHeight)
+            {
+                onGround = true;
+            }
+        }
+    }
+    // not enough with OnCollisionEnter2D. OnCollisionStay2D needed to set onGround properly.
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.name.StartsWith("Brick"))
+        {
+            if (collision.collider.transform.position.y < rb.position.y - halfCatHeight)
+            {
+                onGround = true;
+            }
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.gameObject.name.StartsWith("Brick"))
+        {
+            if (collision.collider.transform.position.y < rb.position.y - halfCatHeight)
+            {
+                onGround = false;
+            }
+        }
     }
 }
